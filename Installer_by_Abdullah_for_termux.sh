528033cba572b49d19ddf7ec5585ca55e967db04
JaDugarALi <ghalibabdullah042@gmail.com>
2017-11-17 23:25:03 +0500

    Add files via upload
    All in one package installer for termux and fedora

---
+++ Installer_by_Abdullah_for_termux.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Abdullah Ghalib. Enjoy"
+echo "---------------------------------------------------------------------------------"
+
+echo "000"
+echo "000"
+echo "000"
+echo "."
+
+echo  "#####################################"
+echo   " Abdullah Ghalib (Ethical Hacker)  "               
+ echo   "This script is written for installing various Important packages and softwares in Termux app "                                                                    
+echo   "####################################"
+
+echo "000"
+echo "000"
+echo "000"
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "#####################################"
+echo   " Abdullah Ghalib (Ethical Hacker)  "               
+ echo   "This script is written for installing various Important packages and softwares in Termux app "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+apt install vim -y
+apt install toilet -y
+
+echo  "#####################################"
+echo   " Abdullah Ghalib (Ethical Hacker)  "               
+ echo   "This script is written for installing various Important packages and softwares in Termux app "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install w3m -y
+apt install boost -y
+
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install proot -y
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install net-tools -y
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+apt install macchanger -y
+apt install cmatrix -y
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+echo " If You are Connected To Internet Then  "
+echo " YOUR PACKAGES ARE INSTALLED SUCCESSFULLY "
+
+exit
---
+++ installerForFedora.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Abdullah Ghalib. Enjoy"
+echo "---------------------------------------------------------------------------------"
+
+echo "000"
+echo "000"
+echo "000"
+echo "."
+
+echo  "#####################################"
+echo   " Abdullah Ghalib (Ethical Hacker)  "               
+ echo   "This script is written for installing various Important packages and softwares in Termux app "                                                                    
+echo   "####################################"
+
+echo "000"
+echo "000"
+echo "000"
+
+echo "."
+
+dnf upgrade -y                      #update in progress
+yum install python -y           #installation......
+yum install python2 -y
+yum install ruby -y 
+yum install git -y
+
+echo  "#####################################"
+echo   " Abdullah Ghalib (Ethical Hacker)  "               
+ echo   "This script is written for installing various Important packages and softwares in Termux app "                                                                    
+echo   "####################################"
+
+yum install php -y 
+yum install perl -y
+yum install nmap -y
+yum install vim -y
+yum install toilet -y
+
+echo  "#####################################"
+echo   " Abdullah Ghalib (Ethical Hacker)  "               
+ echo   "This script is written for installing various Important packages and softwares in Termux app "                                                                    
+echo   "####################################"
+
+yum install bash -y
+yum install clang -y
+yum install jq -y
+yum install w3m -y
+yum install boost -y
+
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+yum install nano -y
+yum install curl -y
+yum install tar -y
+yum install zip -y
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+yum install unzip -y
+yum install tor -y 
+yum install sudo -y
+yum install wget -y
+yum install net-tools -y
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+yum install wcalc -y 
+yum install openssl
+yum install bmon -y
+yum install macchanger -y
+yum install cmatrix -y
+
+echo  "#####################################"
+echo   "  ABDULLAH   GHALIB  (ETHICAL HACKER)     "               
+ echo   " FOR TECH &  ETHICAL HACKING PURPOSES "                                                                   
+echo   "####################################"
+
+echo " If You are Connected To Internet Then  "
+echo " YOUR PACKAGES ARE INSTALLED SUCCESSFULLY "
+
+exit
